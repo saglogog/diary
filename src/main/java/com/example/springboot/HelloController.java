83d67d2d98042312ca327dae0aef2967a1990081
saglogog <saglogog@gmail.com>
2021-05-07 22:00:22 +0300

    test_change_msg

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,14 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Fuck the Boot!";
 	}
 
 }
